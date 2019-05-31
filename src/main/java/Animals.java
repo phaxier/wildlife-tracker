2e4d54ae597efb230c18019fda6d2ed5189c445a
Noel Bor <phaxier@gmail.com>
2019-05-31 18:18:19 +0300

    added test same object is the same

--- src/main/java/Animals.java
+++ src/main/java/Animals.java
@@ -1,2 +1,32 @@
+
+import org.sql2o.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
 public class Animals {
+
+    public Animals(String name, String health, String age,int animalId){
+        this.name = name;
+        this.health = health;
+        this.age = age;
+        this.animalId = animalId;
+    }
+
+    public String name;
+    public String health;
+    public String age;
+    public int animalId;
+
+
+
+public String getName(){return name;}
+
+public String getHealth(){ return health;}
+
+public String getAge(){ return age;}
+
+public int getAnimalId(){ return animalId;}
+
 }
--- src/test/java/AnimalsTest.java
+++ src/test/java/AnimalsTest.java
@@ -1,2 +1,32 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.sql2o.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
 public class AnimalsTest {
-}
+
+    @Test
+    public void animal_instantiatesCorrectly_false() {
+        Animals testAnimal = new Animals("Hyena","okay", "young", 1);
+        assertEquals(true, testAnimal instanceof Animals);
+    }
+    @Test
+    public void Animals_instantiatesWithname_String() {
+        Animals testAnimal = new Animals("Hyena","okay", "young", 1);
+        assertEquals("Hyena", testAnimal.getName());
+    }
+
+    @Test
+    public void client_instantiatesWithNumber_Integer() {
+        Animals newAnimal = new Animals("Hyena", "okay", "young", 1);
+        assertEquals(1, newAnimal.getAnimalId());
+    }
+
+    @Test
+    public void equals_returnsTrueIfTwoObjectsAreSame_true() {
+        Animals testanimal = new Animals("Hyena", "okay", "young", 1);
+        Animals testanimal = new Animals("Hyena", "okay", "young", 1);
+        assertTrue(testanimal.equals(testanimal));
+    }
+    }
