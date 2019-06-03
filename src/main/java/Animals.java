43f99e61e4418b0a3072de2938c3057ca5100562
Noel Bor <phaxier@gmail.com>
2019-06-03 08:55:57 +0300

    update on test if objects are the same

--- src/main/java/Animals.java
+++ src/main/java/Animals.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class Animals {
 
-    public Animals(String name, String health, String age,int animalId){
+    public Animals(String name, String health, String age, int animalId) {
         this.name = name;
         this.health = health;
         this.age = age;
@@ -20,13 +20,62 @@ public class Animals {
     public int animalId;
 
 
+    public String getName() {
+        return name;
+    }
 
-public String getName(){return name;}
+    public String getHealth() {
+        return health;
+    }
 
-public String getHealth(){ return health;}
+    public String getAge() {
+        return age;
+    }
 
-public String getAge(){ return age;}
+    public int getAnimalId() {
+        return animalId;
+    }
 
-public int getAnimalId(){ return animalId;}
+
+    public void save() {
+        try (Connection con = DB.sql2o.open()) {
+            String sql = "INSERT INTO animals (name, health,age, animalid) VALUES (:name, :AnimalId)";
+            this.animalId = (int) con.createQuery(sql, true)
+                    .addParameter("name", this.name)
+                    .addParameter("animalId", this.animalId)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
+
+    @Override
+    public boolean equals(Object otherAnimals) {
+        if (!(otherAnimals instanceof Animals)) {
+            return false;
+        } else {
+            Animals newanimal = (Animals) otherAnimals;
+            return this.getName().equals(newanimal.getName()) &&
+                    this.getAnimalId() == newanimal.getAnimalId();
+        }
+
+        public static List<Animals> all () {
+            String sql = "SELECT * FROM animals";
+            try (Connection con = DB.sql2o.open()) {
+                return con.createQuery(sql).executeAndFetch(Animals.class);x
+            }
+        }
+
+    }
+
+    public static Animals find (int id) {
+        try(Connection con = DB.sql2o.open()){
+            String sql = "SELECT * FROM animals where id=:id";
+            Animals animal = con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(Animals.class);
+            return animal;
+        }
+    }
+
 
 }
--- src/test/java/AnimalsTest.java
+++ src/test/java/AnimalsTest.java
@@ -4,7 +4,12 @@ import org.sql2o.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Arrays;
+
 public class AnimalsTest {
+
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
+
 
     @Test
     public void animal_instantiatesCorrectly_false() {
@@ -12,7 +17,7 @@ public class AnimalsTest {
         assertEquals(true, testAnimal instanceof Animals);
     }
     @Test
-    public void Animals_instantiatesWithname_String() {
+    public void Animals_instantiatesWithName_String() {
         Animals testAnimal = new Animals("Hyena","okay", "young", 1);
         assertEquals("Hyena", testAnimal.getName());
     }
@@ -28,5 +33,54 @@ public class AnimalsTest {
         Animals testmonkey = new Animals("Hyena", "okay", "young", 1);
         Animals testanimal = new Animals("Hyena", "okay", "young", 1);
         assertTrue(testmonkey.equals(testanimal));
+    }
+
+    @Test
+    public void equals_returnsTrueIfNameAndAnimalIdAreSame_true() {
+        Animals testanimal = new Animals("Hyena", "okay", "young", 1);
+        Animals anotheranimal = new Animals("Hyena", "okay", "young", 1);
+        assertTrue(testanimal.equals(anotheranimal));
+    }
+
+
+    @Test
+    public void save_assignsIdToAnimal() {
+        Animals testanimal = new Animals("Hyena", "okay", "young", 1);
+        testanimal.save();
+        Animals savedanimal = Animals.all().get(0);
+        assertEquals(savedanimal.getAnimalId(), testanimal.getAnimalId());
+    }
+
+    @Test
+    public void all_returnsAllInstancesOfAnimal_true() {
+        Animals firstanimal = new Animals("Hyena", "okay", "young", 1);
+        firstanimal.save();
+        Animals secondanimal = new Animals("Hyena", "okay", "young", 1);
+        secondanimal.save();
+        assertEquals(true, Animals.all().get(0).equals(firstanimal));
+        assertEquals(true, Animals.all().get(1).equals(secondanimal));
+    }
+
+    @Test
+    public void find_returnsAnimalWithSameid_secondAnimal() {
+        Animals firstanimal = new Animals("Hyena", "okay", "young", 1);
+        firstanimal.save();
+        Animals secondanimal = new Animals("Hyena", "okay", "young", 1);
+        secondanimal.save();
+        assertEquals(Animals.find(secondanimal.getAnimalId()), secondanimal);
+
+    }
+
+    @Test
+    public void getAnimal_retrievesAllAnimalsFromDatabase_animalList() {
+        Animals testanimal = new Animals("Hyena", "okay", "young", 1);
+        testanimal.save();
+        Animals firstanimal = new Animals("monkey", "nice", "adult, 2", testanimal.getAnimalId());
+        firstanimal.save();
+        Animals secondanimal = new Animals("monkey", "nice", "adult, 2", testanimal.getAnimalId());
+        secondanimal.save();
+        Animals[] animals = new Animals[]{firstanimal, secondanimal};
+        assertTrue(testanimal.getanimal().containsAll(Arrays.asList(animals)));
+
     }
     }
